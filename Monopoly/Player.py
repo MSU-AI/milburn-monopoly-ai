d85e51651ae1a8c84f9fc85220fd5b188afe56ff
makibrte <milicev2@msu.edu>
2022-10-18 20:56:36 -0400

    Added Skeleton for Player Class(Unfinished)

--- Monopoly/Player.py
+++ Monopoly/Player.py
@@ -1,6 +1,28 @@
-
+# TODO: Import all the other classes in here so you can use them
 class Player:
     """
     Represents a player in the Monopoly game.
     """
+    # TODO: initialize the player so that he has default starting money, position
+    # TODO: Also initialize list for properties 
+    def __init__(self):
+        pass
+    # TODO: FINISH METHODS BELOW
+    def rollDice():
+        pass
+    def updatePos():
+        pass
+    def buyProperty():
+        pass
+    def sellProperty():
+        pass 
+    def draw_Community_Chest():
+        pass
+    def draw_Chance():
+        pass
+    def pay_Rent():
+        pass
+    def pay_Tax():
+        pass
+    
     pass
