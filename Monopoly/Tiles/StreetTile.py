967048b4ab44b4247b945ab46ab3de5cb89a033a
Nicholas Ly <nichdly@gmail.com>
2022-10-20 21:49:07 -0400

    Implemented skeleton code for new Tile system

---
+++ Monopoly/Tiles/StreetTile.py
@@ -0,0 +1,13 @@
+
+from Tile import Tile
+
+class StreetTile(Tile):
+    """
+    Represents a street tile on the board.
+    """
+
+    def __init__(self):
+        """
+        Initializes the street tile.
+        """
+        super().__init__()
---
+++ Monopoly/Tiles/TaxTile.py
@@ -0,0 +1,13 @@
+
+from Tile import Tile
+
+class TaxTile(Tile):
+    """
+    Represents a tax tile on the board.
+    """
+
+    def __init__(self):
+        """
+        Initializes the tax tile.
+        """
+        super().__init__()
