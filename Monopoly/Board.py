5a92a1593a8c170e700569b0c33421ded77354fd
Nicholas Ly <nichdly@gmail.com>
2022-10-22 19:01:48 -0400

    Finished Board.py

--- Monopoly/Board.py
+++ Monopoly/Board.py
@@ -16,26 +16,17 @@ import pygame
 
 pygame.font.init()
 
-# # TODO: CREATE A FUNCTION THAT INITIALIZES ALL THE PROPERTIES
-# # CHECK OUT THE IPYNB IN THE ROOT DIRECOTRY TO SEE HOW THE DATA FRAME LOOKS, YOU PROBABLY WANT TO SEPARATE CREATION OF OBJECTS DEPENDING ON IS IT A STREET,
-# # UTILITY OR RAIL ROAD
-# # QUICK REMAINDER, COLUMN IN THE DATA FRAME RentBuild5 MEANS RENT WHEN HOTEL IS BUILT, THINK OF A WAY TO IMPLEMENT THAT,
-# # MY SUGGESTION WOULD BE AN ARRAY OF PRICES AS ATTRIBUTE AND DEPENDING ON NUM OF BUILDS RETRIEVE CORRESPONDING VALUE
-# # FOR EXAMPLE: BALTIC AVENUE -> RENTS[4,20,60,180,320,450] , BUILD = 3, RENT = RENTS[BUILD]
-
-# def initProperty(df):
-#     pass
-
 class Board:
     """
     Represents the Monopoly game board.
     """
     
     COLORS = {
-        'board' : (219, 219, 219),
-        'black' : (0, 0, 0)
+        'Black' : (0, 0, 0),
+        'LightGray' : (219, 219, 219)
     }
-    FONT = pygame.font.SysFont(pygame.font.get_fonts()[73], 100)  # Title font
+
+    FONT = pygame.font.SysFont('segoeui', 100)  # Title font
 
     def __init__(self):
         """
@@ -44,53 +35,55 @@ class Board:
         # Initializes board as a list of the board's sides and tiles.
         self.board = self.make_board()
 
-    def draw(self, window, pad):
+    def draw(self, window: pygame.Surface):
         """
         Draws the game board. 
         """
         # Fills screen with background color, wiping the screen of artifacts
-        window.fill(self.COLORS['board'])
+        window.fill(self.COLORS['LightGray'])
 
         # Renders title header
-        title = self.FONT.render( "monopoly", 1, self.COLORS['black'] )
+        title = self.FONT.render( "monopoly", 1, self.COLORS['Black'] )
         window.blit( title, (
             window.get_width() / 2 - title.get_width() / 2,
             window.get_height() / 2 - title.get_height() / 2
         ))
 
+        padding = 20  # Distance between board and window
+        board_width = window.get_width() - padding * 2  # Board width
+        board_height = window.get_height() - padding * 2  # Board height
+
         # Draw board tiles
         for s, side in enumerate(self.board):
             for t, tile in enumerate(side):
 
-                # TODO: Math here is super janky; probably could be simplified.
-
-                # Tiles are drawn on their side accordingly
+                # Tiles are drawn on their according side
                 if s == 0:
                     tile.draw(
                         window,
-                        t * tile.WIDTH + pad,
-                        pad
+                        padding + t * tile.WIDTH,
+                        padding
                     )
                 elif s == 1:
                     tile.draw(
                         window,
-                        window.get_width() - pad * 2 - tile.WIDTH + pad,
-                        t * tile.HEIGHT + pad
+                        padding + board_width - tile.WIDTH,
+                        padding + t * tile.HEIGHT
                     )
                 elif s == 2:
                     tile.draw(
                         window,
-                        window.get_width() - pad * 2 - tile.WIDTH + pad - t * tile.WIDTH,
-                        window.get_height() - pad * 2 - tile.HEIGHT + pad
+                        padding + board_width - (t + 1) * tile.WIDTH,
+                        padding + board_height - tile.HEIGHT
                     )
                 elif s == 3:
                     tile.draw(
                         window,
-                        pad,
-                        window.get_height() - pad - (t + 1) * tile.HEIGHT
+                        padding,
+                        padding + board_height - (t + 1) * tile.HEIGHT
                     )
 
-    def make_board(self):
+    def make_board(self) -> list[list[Tile]]:
         """
         Reads board information from CSV file.
         """
@@ -102,7 +95,7 @@ class Board:
             attributes = dict(row)
 
             # TODO: Currently uses default (empty) Tile constructors; should be
-            #       changed to take attributes as an argument (see line 110).
+            #       changed to take attributes as an argument (see StreetTile).
 
             if row['Space'] == "Go":
                 board[side].append( GoTile() )
