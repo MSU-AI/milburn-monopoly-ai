73204f348e93b89405da13daf681103beca6e469
RyanFilipiak <114832068+RyanFilipiak@users.noreply.github.com>
2022-10-30 14:54:22 -0400

    Update ChanceTile.py

--- Monopoly/Tiles/ChanceTile.py
+++ Monopoly/Tiles/ChanceTile.py
@@ -16,46 +16,97 @@ class ChanceTile(Tile):
         self.players = np.array([])
         super().__init__()        
         
+        
     def goToGo(player):
         player.pos = np.array([0,0])
         player.getMoney(200)
     
+    
     def goIllinoisAve(player):
-        #only get 200 if they pass go
-        player.pos = np.array([2,4])
-        player.getMoney(200)
+        #need to check players current position to see if they pass go
+        if (player.pos == [0,7]):
+            player.pos = np.array([2,4])
+        
+        elif (player.pos == [2,2]):
+            player.pos = np.array([2,4])
+        
+        elif (player.pos == [3,6])
+            player.pos = np.array([2,4])
+            player.getMoney(200)
 
-    def goCharlesPlace(player):
-        #only get 200 if they pass go
-        player.pos = np.array([1,1])
-        player.getMoney(200)
 
-    def goNearestUtility(player):
-        pass
+    def goCharlesPlace(self, player):
+        #need to check players current position to see if they pass go
+        if (player.pos == [0,7]):
+            player.pos = np.array([1,1])
+        
+        elif (player.pos == [2,2]):
+            player.pos = np.array([1,1])
+            player.getMoney(200)
+            
+        
+        elif (player.pos == [3,6])
+            player.pos = np.array([1,1])
+            player.getMoney(200)
+
+
+    def goNearestUtility(self, player):
+        #need to check players current position to find closest ulitily
+        if (player.pos == [0,7]):
+            player.pos = np.array([1,2])
+        
+        elif (player.pos == [2,2]):
+            player.pos = np.array([2,8])
+        
+        elif (player.pos == [3,6])
+            player.pos = np.array([1,2])    
+    
     
     def goNearestRailRoad(player):
-        pass
+        #need to check players current position to find closest railroad
+        if (player.pos == [0,7]):
+            player.pos = np.array([1,5])
+        
+        elif (player.pos == [2,2]):
+            player.pos = np.array([2,5])
+        
+        elif (player.pos == [3,6])
+            player.pos = np.array([0,5])      
     
     def bankDividends(player):
         player.addMoney(200)
+        
     
     def getOutOfJail(player):
         player.addGetOutOfJail()
         
+        
     def goToJail(player):
         player.updatePos([1,0])
         
+        
     def goBackThree(player):
-        pass
+        #need to check players current position to see which tile they go back to
+        if (player.pos == [0,7]):
+            player.pos = np.array([0,4])
+        
+        elif (player.pos == [2,2]):
+            player.pos = np.array([1,9])
+        
+        elif (player.pos == [3,6])
+            player.pos = np.array([3,3])
     
     def generalRepairs(player):
         player.payTax(price = player.houses * 25 + player.hotels * 100)
         
+        
     def poorTax(player):
         player.payTax(price = 15)
         
+        
     def goReadingRailroad(player):
         player.pos = np.array([0,5])
+        
         
     def goBoardwalk(player):
         player.pos = np.array([3,9])
@@ -65,8 +116,11 @@ class ChanceTile(Tile):
             if(payer != player):
                 player.payTax(payer, 50)
                 
+                
     def buildingLoan(player):
         player.addMoney(150)
         
+        
     def crosswordWin(player):
         player.addMoney(100)
+        
