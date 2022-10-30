c04fa29a9566318b59ea918caeb3559dfd1348b9
RyanFilipiak <114832068+RyanFilipiak@users.noreply.github.com>
2022-10-30 14:00:52 -0400

    Update ChanceTile.py

--- Monopoly/Tiles/ChanceTile.py
+++ Monopoly/Tiles/ChanceTile.py
@@ -1,6 +1,7 @@
-from Tiles.Tile import Tile
-from Bank import Bank
+import numpy as np
+from Tile import Tile
 from Player import Player
+from JailTile import JailTile
 
 class ChanceTile(Tile):
     """
@@ -11,12 +12,61 @@ class ChanceTile(Tile):
         """
         Initializes the "Chance" tile.
         """
-        super().__init__()
+        self.deck = []
+        self.players = np.array([])
+        super().__init__()        
         
-    def drawCard(self):
-        #where a random card will be draw by the player
-        #unsure of how to draw random chance card
-        #unsure of how to implement actions of random chance card
-        #https://github.com/LiYChristopher/cli-monopoly/blob/master/cards.csv 
-        #above link cotains a csv of the community chest and chance cards
+    def goToGo(player):
+        player.pos = np.array([0,0])
+        player.getMoney(200)
+    
+    def goIllinoisAve(player):
+        #only get 200 if they pass go
+        player.pos = np.array([2,4])
+        player.getMoney(200)
+
+    def goCharlesPlace(player):
+        #only get 200 if they pass go
+        player.pos = np.array([1,1])
+        player.getMoney(200)
+
+    def goNearestUtility(player):
         pass
+    
+    def goNearestRailRoad(player):
+        pass
+    
+    def bankDividends(player):
+        player.addMoney(200)
+    
+    def getOutOfJail(player):
+        player.addGetOutOfJail()
+        
+    def goToJail(player):
+        player.updatePos([1,0])
+        
+    def goBackThree(player):
+        pass
+    
+    def generalRepairs(player):
+        player.payTax(price = player.houses * 25 + player.hotels * 100)
+        
+    def poorTax(player):
+        player.payTax(price = 15)
+        
+    def goReadingRailroad(player):
+        player.pos = np.array([0,5])
+        
+    def goBoardwalk(player):
+        player.pos = np.array([3,9])
+        
+    def boardChairman(player, players):
+        for payer in players:
+            if(payer != player):
+                player.payTax(payer, 50)
+                
+    def buildingLoan(player):
+        player.addMoney(150)
+        
+    def crosswordWin(player):
+        player.addMoney(100)
